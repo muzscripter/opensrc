d03a57191e1869cd318d6213c645342372b8f925
muzscripter <109730545+muzscripter@users.noreply.github.com>
2023-04-02 19:20:54 -0400

    Update player_spin.lua

--- aim_manipulation/player_spin.lua
+++ aim_manipulation/player_spin.lua
@@ -1,15 +1,16 @@
+
 local player = game.Players.LocalPlayer
 local humanoid = player.Character:WaitForChild("Humanoid")
 
-humanoid.AutoRotate = false
-
 local spinTime = 2 -- number of seconds to spin
-local spinSpeed = 10 -- degrees per second to spin
+local spinSpeed = 180 -- degrees per second to spin
 
-for i = 1, spinTime * spinSpeed do
-    humanoid.RootPart.CFrame *= CFrame.Angles(0, math.rad(spinSpeed), 0) 
+local startTime = tick()
+
+while tick() < startTime + spinTime do
+    local delta = math.clamp((tick() - startTime) / spinTime, 0, 1) 
+    humanoid.RootPart.CFrame = humanoid.RootPart.CFrame * CFrame.Angles(0, math.rad(spinSpeed * delta), 0) 
     wait()
 end
 
 humanoid.WalkSpeed = 16 -- reset the character's walkspeed
-humanoid.AutoRotate = true -- reset automatic rotation
