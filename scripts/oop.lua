9a3b008962c7c2b2a3c24a741976c1562127b74d
graverealm <109730545+muzscripter@users.noreply.github.com>
2024-03-18 10:27:41 -0400

    Update oop.lua

--- scripts/oop.lua
+++ scripts/oop.lua
@@ -1,9 +1,10 @@
+type F = (...number) -> ...string
 export type pair<pair_type> = {
     [pair_type] :: unknown = {};
     [pair_type] :: unknown = {};
 }
 
-local function return_pair(types)
+local function return_pair(...: {types}): {types}
     if typeof(types) == 'table' then
         if #types > 2 then return end
         local type_tbl: pair<types[#types - 1]> = {First = 'Test', Second = 'Test2'}
